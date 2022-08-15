4f28ff6887a4a2c9781f9d1631ec3225a5bcb999
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-08-15 22:49:08 +0200

    project initialization

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from '../../assets/bloobloom.png'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>Menu</h1>
+        <img src={Image} alt='bloobloomIcon'/>
+    </div>
+  )
+}
+
+export default Navbar;
